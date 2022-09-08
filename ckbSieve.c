132a2fa6e6a611df76224d75f0ab8ee9fad8a55b
mosgiel <100488716+mosgiel@users.noreply.github.com>
2022-09-08 15:29:54 -0700

    Update ckbSieve.c

--- ckbSieve.c
+++ ckbSieve.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <time.h>
 #include <math.h>
-#define WIDTH 746911
 
 ///Variable Declaration
 int factorLimit = 100000;
-int valueLimit = 1753089;
-bool domain[WIDTH] = {[0 ... WIDTH - 1] = true};
 int cycle;
 int offset = -1;
-int s = 0;
 
 int detectCycleOffset(int x) {
     int m = 1;
@@ -26,26 +21,7 @@ int detectCycleOffset(int x) {
     }
 }
 
-void updateDomain() {
-    if (offset != -1) {
-        int d = 0;
-        for (int i = 0; i < cycle; ++i) {
-            if ((valueLimit + i) % cycle == offset) {
-                d = i;
-            }
-        }
-        while (d < WIDTH) {
-            if (domain[d] == true) {
-                ++s;
-            }
-            domain[d] = false;
-            d += cycle;
-        }
-    }
-}
-
 bool checkPrime(int x) {
-
     float y = x;
     for (int i = 2; i <= sqrt(y); ++i) {
         if (x % i == 0) {
@@ -57,9 +33,6 @@ bool checkPrime(int x) {
 
 int main(void) {
     ///Miscellaneous Management
-    time_t t1;
-    t1 = time(NULL);
-
     FILE *fp;
     fp = fopen("results.txt", "w");
 
@@ -69,24 +42,13 @@ int main(void) {
     while (i < factorLimit) {
         if (checkPrime(k) == true) {
             detectCycleOffset(k);
-            updateDomain();
+            printf("(%d, %d)", cycle, offset);
+            fprintf(fp, "(%d, %d)", cycle, offset);
         }
         cycle = 0;
         offset = -1;
         k += 2;
         ++i;
-        if (i % (factorLimit / 100) == 0) {
-            time_t t2;
-            t2 = time(NULL);
-            printf("\rAmount Sieved: %d, Candidates Removed: %d, Time: %d", i, s, t2 - t1);
-        }
-    }
-    printf("\r");
-    for (int i = 0; i < WIDTH; ++i) {
-        if (domain[i] == true) {
-            printf("%d ", i + valueLimit);
-            fprintf(fp, "," + i + valueLimit);
-        }
     }
     return 0;
 }
